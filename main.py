93006266fc8082f0986604d9a75b9401f6b658cc
Atulya-Singh <97362243+singhatulya@users.noreply.github.com>
2025-11-28 17:44:21 +0530

    added the car sprite :)

--- main.py
+++ main.py
@@ -8,8 +8,13 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
+    car1 = pygame.image.load("car1.png")
+    car1 = pygame.transform.scale(car1, (50,100))
+    carx = 640
+    cary = 360
     screen.fill((0,255,50))
+
+    screen.blit(car1, (carx,cary))
     pygame.display.update()
 
 pygame.quit()
